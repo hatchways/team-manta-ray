69c56cc9079405c9b2012b890cd088ef748438da
Mina Mina <minakhamesi@gmail.com>
2021-03-10 14:47:08 -0500

    Incorporate prettier settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import {MuiThemeProvider} from '@material-ui/core';
-import {BrowserRouter, Route, Redirect} from 'react-router-dom';
+import React from "react";
+import { MuiThemeProvider } from "@material-ui/core";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 
-import {theme} from './themes/theme';
-import Login from './pages/AuthRoutes/pages/Login';
-import Signup from './pages/AuthRoutes/pages/Signup';
+import { theme } from "./themes/theme";
+import Login from "./pages/AuthRoutes/pages/Login";
+import Signup from "./pages/AuthRoutes/pages/Signup";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-	return (
-		<MuiThemeProvider theme={theme}>
-			<BrowserRouter>
-				<Route exact path='/signup' component={Signup} />
-				<Route exact path='/login' component={Login} />
-				<Route exact path='/'>
-					<Redirect to='/signup' />
-				</Route>
-			</BrowserRouter>
-		</MuiThemeProvider>
-	);
+  return (
+    <MuiThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/">
+          <Redirect to="/signup" />
+        </Route>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
--- client/src/themes/theme.js
+++ client/src/themes/theme.js
@@ -1,15 +1,15 @@
-import {createMuiTheme} from '@material-ui/core';
+import { createMuiTheme } from "@material-ui/core";
 
 export const theme = createMuiTheme({
-	typography: {
-		fontFamily: ['Montserrat', 'sans-serif'].join(','),
-		fontSize: 12,
-		h1: {
-			// could customize the h1 variant as well
-		},
-	},
-	palette: {
-		primary: {main: '#DF1B1B'},
-		secondary: {main: '#FF510C'},
-	},
+  typography: {
+    fontFamily: ["Montserrat", "sans-serif"].join(","),
+    fontSize: 12,
+    h1: {
+      // could customize the h1 variant as well
+    },
+  },
+  palette: {
+    primary: { main: "#DF1B1B" },
+    secondary: { main: "#FF510C" },
+  },
 });
