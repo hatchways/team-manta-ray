c0f463970e8fa2a543635e6e7e211ab0422971ef
Mina Mina <minakhamesi@gmail.com>
2021-03-10 14:41:14 -0500

    Apply prettier settings to branch-profile file

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -1,59 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const recipeSchema = Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	pictureUrl: {
-		type: String,
-		required: true,
-	},
-	price: {
-		type: Number,
-		required: true,
-	},
-	ingredients: {
-		type: [String],
-		required: true,
-	},
-	requiredStuff: {
-		type: [String],
-	},
-	portionDescription: {
-		type: String,
-		required: true,
-	},
-	cuisineTags: {
-		type: [String],
-		required: true,
-	},
+  name: {
+    type: String,
+    required: true,
+  },
+  pictureUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ingredients: {
+    type: [String],
+    required: true,
+  },
+  requiredStuff: {
+    type: [String],
+  },
+  portionDescription: {
+    type: String,
+    required: true,
+  },
+  cuisineTags: {
+    type: [String],
+    required: true,
+  },
 });
 
 const profileSchema = new Schema({
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	location: {
-		type: String,
-		required: true,
-	},
-	bio: String,
-	favCuisines: {
-		type: [String],
-		required: true,
-	},
-	profilePictureUrl: {
-		type: String,
-		required: true,
-	},
-	recipes: [recipeSchema],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  bio: String,
+  favCuisines: {
+    type: [String],
+    required: true,
+  },
+  profilePictureUrl: {
+    type: String,
+    required: true,
+  },
+  recipes: [recipeSchema],
 });
 
-const Profile = mongoose.model('Profile', profileSchema);
+const Profile = mongoose.model("Profile", profileSchema);
 
 module.exports = Profile;
