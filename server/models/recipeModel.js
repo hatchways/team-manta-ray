4e51f67b7f886c84efe7a0ed4845e582cdfeb018
Mina Mina <minakhamesi@gmail.com>
2021-03-10 16:15:03 -0500

    Add userField in recipeModel

--- server/models/recipeModel.js
+++ server/models/recipeModel.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
   name: {
     type: String,
     required: true,
