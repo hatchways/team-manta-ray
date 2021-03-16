7a9ffb34d02bcdc3512cf9d32ee0b94310e6d9e8
Mina Mina <minakhamesi@gmail.com>
2021-03-16 15:44:12 -0400

    Incorporate backend routes into context actions

--- client/src/actions/recipeActions.js
+++ client/src/actions/recipeActions.js
@@ -4,6 +4,9 @@ import {
   CREATE_RECIPE_SUCCESS,
   CREATE_RECIPE_FAIL,
   EDIT_RECIPE,
+  GET_RECIPE,
+  DELETE_RECIPE,
+  GET_RECIPES_BY_CHEF,
 } from "../constants/RecipeConstants";
 
 export const createRecipe = async (dispatch, payload) => {
@@ -28,7 +31,7 @@ export const createRecipe = async (dispatch, payload) => {
     };
 
     const res = await axios.post("/api/recipes", formData, config);
-    if (res.status === 200) {
+    if (res.status === 201) {
       dispatch({
         type: CREATE_RECIPE_SUCCESS,
         payload: { ...payload, id: Math.floor(Math.random() * 10000) },
@@ -42,9 +45,55 @@ export const createRecipe = async (dispatch, payload) => {
 };
 
 export const editRecipe = async (dispatch, payload) => {
-  dispatch({ type: EDIT_RECIPE, payload });
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const formData = {
+      name: payload.name,
+      price: payload.price,
+      ingredients: payload.ingredients,
+      requiredStuff: payload.requiredStuff,
+      portionDescription: payload.portionDescription,
+      cuisineTags: payload.cuisineTags,
+      pictureKey: payload.pictureKey,
+    };
+    const res = await axios.put(
+      `/api/recipes/${payload._id}`,
+      formData,
+      config
+    );
+    if (res.status === 200) {
+      dispatch({ type: EDIT_RECIPE, payload });
+    }
+  } catch (error) {}
 };
 
-export const deleteRecipe = async (dispatch, payload) => {};
+export const deleteRecipe = async (dispatch, recipeId) => {
+  try {
+    const res = await axios.delete(`/api/recipes/${recipeId}`);
+    if (res.status === 200) {
+      dispatch({ type: DELETE_RECIPE, payload: recipeId });
+    }
+  } catch (error) {}
+};
 
-export const getRecipesByChef = async (dispatch, payload) => {};
+export const getRecipesByChef = async (dispatch, chefId) => {
+  try {
+    const res = await axios.get(`/api/recipes/chef/${chefId}`);
+    if (res.status === 200) {
+      dispatch({ type: GET_RECIPES_BY_CHEF, payload: res.data });
+    }
+  } catch (error) {}
+};
+
+export const getRecipeById = async (dispatch, recipeId) => {
+  try {
+    const res = await axios.get(`/api/recipes/${recipeId}`);
+    if (res.status === 200) {
+      dispatch({ type: GET_RECIPE, payload: res.data });
+    }
+  } catch (error) {}
+};
--- client/src/reducers/RecipeReducer.js
+++ client/src/reducers/RecipeReducer.js
@@ -1,25 +1,47 @@
 import {
   CREATE_RECIPE_SUCCESS,
+  DELETE_RECIPE,
   EDIT_RECIPE,
+  GET_RECIPE,
+  GET_RECIPES_BY_CHEF,
 } from "../constants/RecipeConstants";
 
 export const initialState = {
   recipes: [],
+  recipe: {},
 };
 
 export const RecipeReducer = (state, action) => {
   switch (action.type) {
     case CREATE_RECIPE_SUCCESS:
-      return { recipes: [action.payload, ...state.recipes] };
+      return { ...state, recipes: [action.payload, ...state.recipes] };
     case EDIT_RECIPE:
       return {
+        ...state,
         recipes: state.recipes.map((recipe) => {
-          if (recipe.id === action.payload.id) {
+          if (recipe._id === action.payload._id) {
             return action.payload;
           } else {
             return recipe;
           }
         }),
+      };
+    case DELETE_RECIPE:
+      return {
+        ...state,
+        recipes: state.recipes.filter(
+          (recipe) => recipe._id !== action.payload
+        ),
+      };
+    case GET_RECIPE:
+      return {
+        ...state,
+        recipe: action.payload,
+      };
+    case GET_RECIPES_BY_CHEF:
+      return {
+        ...state,
+        recipes: action.payload,
       };
     default:
       return state;
