3235de0565875a14843106d318e90b5661aac0e4
Mina Mina <minakhamesi@gmail.com>
2021-03-16 14:10:06 -0400

    Delete a test route that no longer is needed

--- server/routes/testRecipeRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", async (req, res) => {
-  console.log("reaching end Point");
-  res.json(req.body);
-});
-
-module.exports = router;
