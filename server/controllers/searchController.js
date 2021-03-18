559df3b5cf8721b70deb80f12c05fe7afef5041c
Yassine Bounekhla <rudream12@gmail.com>
2021-03-18 11:52:01 -0400

    :memo: Added some documentationn on how to use the filters

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,6 +1,25 @@
 const Recipe = require("../models/recipeModel");
 const ChefProfile = require("../models/chefProfileModel");
 
+/**
+ * @description Filter and get recipes and chefs based on filters
+ * @route GET /api/search
+ * @access Public
+ *
+ * @params
+ * Parameter:      Expected:             Description:
+ *
+ * &cuisines=  eg. ["asian","japanese"]  The tags to use to filter recipes and chefs. Will include all chefs and recipes whose cuisineTags match at least one tag.
+ * &chef=          true/false            True/false to determine whether to retrieve chefs or recipes. If true, will return chefs, if false or ommitted, will return recipes.
+ * &location=      [lng,lat]             Array of coordinates that will be used in all filters relating to distance, such as max distance for chefs
+ * &maxdistance=   eg. 30                A number in kilometers that will determine the maximum distance returned chefs can be from the user (requires &location to work)
+ * &sortby=        eg. price             What metric to use to sort the results (if ommitted, they are not sorted).
+ * &order=         asc/desc              Determines in what order to sorted results should appear (requires &sortby to work)
+ * &page=          eg. 2                 Which page of the data should be returned (if ommitted, page 1 is returned)
+ * &limit=         eg.  10               The maximum number of results per page (if ommitted, defaults to 12 items per page)
+ *
+ * ALl parameters are optional. If all are ommitted, the response will be the first page of the collection of all recipes.
+ */
 const getFiltered = async (req, res) => {
   try {
     const mongoQuery = {};
