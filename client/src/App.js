c457932f55012ba08d37532203552bf9b1327875
Vaibhav-Kambli <vaibhavk2608@gmail.com>
2021-03-22 19:53:49 -0400

    Added protected routes for logged in user

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,41 @@
 import React from "react";
 import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { theme } from "./themes/theme";
 import RecipeContextProvider from "./context/RecipeContext";
-
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import CustomerProfile from "./pages/CustomerProfile";
-// import NavBar from "./components/NavBar";
 import "./App.css";
 import { ContextProvider } from "./context/UserContext";
 import TestComponent from "./pages/TestComponent";
 import SuccessPage from "./pages/SuccessPage";
-// import TestChefProfile from "./pages/TestChefProfile";
 
 // stripe imports
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import Payment from "./pages/Payment";
 import ChefProfile from "./pages/ChefProfile";
+import ProtectedRoutes from "./routes/ProtectedRoutes";
+import NavBar from "./components/NavBar";
 
 /** This will be moved once we have a parent component for payment */
 const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const Private = () => {
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route path="/success" component={SuccessPage} />
+        <Route path="/payment" render={(props) => <Payment {...props} />} />
+        <Route path="/test" component={TestComponent} />
+        <Route path="/profile" component={CustomerProfile} />
+        <Route path="/chefprofile" component={ChefProfile} />
+      </Switch>
+    </>
+  );
+};
 
 function App() {
   return (
@@ -31,22 +45,9 @@ function App() {
           <RecipeContextProvider>
             <Elements stripe={promise}>
               <Switch>
-                <Route path="/profile" component={CustomerProfile} />
-
-                <Route path="/test" component={TestComponent} />
-                <Route path="/success" component={SuccessPage} />
-                {/** This will be moved once we have a parent component for payment */}
-                <Route
-                  path="/payment"
-                  render={(props) => <Payment {...props} />}
-                />
-                <Route path="/chefprofile" component={ChefProfile} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-
-                <Route path="/" component={Login} exact>
-                  <Redirect to="/login" />
-                </Route>
+                <Route path="/login" component={Login} exact />
+                <Route path="/signup" component={Signup} exact />
+                <ProtectedRoutes path="/" component={Private} />
               </Switch>
             </Elements>
           </RecipeContextProvider>
