a8353d954b76f506eb7fcac184fa793fdcf09648
Yassine Bounekhla <rudream12@gmail.com>
2021-03-23 10:48:53 -0400

    :feature: Created conversation model

---
+++ server/models/conversationModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const conversationSchema = new Schema({
+  participants: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  messages: [
+    {
+      content: {
+        type: String,
+        max: 500, //Max 500 characters
+      },
+      sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      createdAt: { type: Date },
+    },
+  ],
+});
+
+const Conversation = mongoose.model("Conversation", conversationSchema);
+
+module.exports = Conversation;
