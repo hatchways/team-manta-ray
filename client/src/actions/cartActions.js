e2723ec6d26f2f9d8ab69274c8fea0b5f71d774d
Vaibhav-Kambli <vaibhavk2608@gmail.com>
2021-03-25 13:08:35 -0400

    Removed commented code

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -2,7 +2,7 @@ import {
   CART_SAVE_SHIPPING_ADDRESS,
   CART_SAVE_BOOKING_DATE,
 } from "../constants/cartConstants";
-// export const login = async (dispatch, loginPayload) => {
+
 export const saveShippingAddress = async (dispatch, data) => {
   dispatch({
     type: CART_SAVE_SHIPPING_ADDRESS,
