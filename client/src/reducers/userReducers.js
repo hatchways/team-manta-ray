23fa8f494944712a37ac016698887f1f84078355
Vaibhav-Kambli <vaibhavk2608@gmail.com>
2021-04-01 10:38:44 -0400

    Removed unused reducer function

--- client/src/reducers/userReducers.js
+++ client/src/reducers/userReducers.js
@@ -13,7 +13,6 @@ import {
   USER_UPDATE_REQUEST,
   USER_UPDATE_SUCCESS,
   USER_UPDATE_FAIL,
-  USER_UPDATE_RESET,
   USER_UPDATE_PASSWORD_REQUEST,
   USER_UPDATE_PASSWORD_SUCCESS,
   USER_UPDATE_PASSWORD_FAIL,
@@ -113,9 +112,6 @@ export const UserUpdateReducer = (initialState, action) => {
         loading: false,
         error: action.payload,
       };
-
-    case USER_UPDATE_RESET:
-      return {};
 
     default:
       return initialState;
